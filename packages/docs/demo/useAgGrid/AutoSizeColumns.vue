3acb835a68e80a48ad7d9446faf9e63df23fe543
xyl <1031824970@qq.com>
2024-11-11 16:26:20 +0800

    fix 修改文件夹大小写命名

---
+++ packages/docs/demo/useAgGrid/AutoSizeColumns.vue
@@ -0,0 +1,141 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { AgGridVue } from '@ag-grid-community/vue3'
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model'
+import '@ag-grid-community/styles/ag-grid.css'
+import '@ag-grid-community/styles/ag-theme-alpine.css'
+import { useAgGrid } from '@x-anything/hooks'
+const modules = [ClientSideRowModelModule]
+
+const rowData = ref<any[]>([])
+
+const colDefs = ref([
+  {
+    field: 'make',
+    tooltipField: 'make',
+    headerName: 'MAKEEEEEE',
+    pinned: 'left',
+  },
+  { field: 'model' },
+  { field: 'price', tooltipField: 'price' },
+  { field: 'electric', pinned: 'right' },
+])
+
+setTimeout(() => {
+  rowData.value = [
+    {
+      make: 'Tesla',
+      model:
+        'Model Y Model Y Model Y Model Y Model Y Model Y Model Y Model Y Model Y Model Y Model Y',
+      price: `649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950`,
+      electric: '按钮 按钮',
+    },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: '按钮 按钮' },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+    {
+      make: 'Toyota',
+      model: 'Corolla',
+      price: 29600,
+      electric: '按钮 按钮 按钮 按钮',
+    },
+  ]
+}, 100)
+
+const { gridReady, firstDataRendered } = useAgGrid({
+  fitCellContents: true,
+  maxWidth: 300,
+  autoSizeColumns: ['electric'],
+})
+</script>
+
+<template>
+  <ag-grid-vue
+    :modules="modules"
+    :rowData="rowData"
+    :columnDefs="colDefs"
+    class="ag-theme-alpine"
+    style="height: 300px"
+    :tooltipInteraction="true"
+    :tooltipShowDelay="500"
+    @grid-ready="gridReady"
+    @first-data-rendered="firstDataRendered"
+  >
+  </ag-grid-vue>
+</template>
---
+++ packages/docs/demo/useAgGrid/MaxWidth.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { AgGridVue } from '@ag-grid-community/vue3'
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model'
+import '@ag-grid-community/styles/ag-grid.css'
+import '@ag-grid-community/styles/ag-theme-alpine.css'
+const modules = [ClientSideRowModelModule]
+
+const rowData = ref<any[]>([])
+
+const colDefs = ref([
+  { field: 'make', tooltipField: 'make', headerName: 'MAKEEEEEE' },
+  { field: 'model' },
+  { field: 'price', tooltipField: 'price' },
+  { field: 'electric', pinned: 'right' },
+])
+
+setTimeout(() => {
+  rowData.value = [
+    {
+      make: 'Tesla',
+      model:
+        'Model Y Model Y Model Y Model Y Model Y Model Y Model Y Model Y Model Y Model Y Model Y',
+      price: `649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950649506495064950`,
+      electric: true,
+    },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ]
+}, 100)
+</script>
+
+<template>
+  <ag-grid-vue
+    :modules="modules"
+    :rowData="rowData"
+    :columnDefs="colDefs"
+    class="ag-theme-alpine"
+    style="height: 300px"
+    :tooltipInteraction="true"
+    :tooltipShowDelay="500"
+    :autoSizeStrategy="{
+      type: 'fitCellContents',
+    }"
+    :defaultColDef="{
+      maxWidth: 150,
+    }"
+  >
+  </ag-grid-vue>
+</template>
